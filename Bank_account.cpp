269aab84c2788f9ef616e80489c6cc340faa25ba
Umneiah <omnia.hussien@ymail.com>
2015-10-22 21:25:34 +0200

    Update Bank_account.cpp

--- Bank_account.cpp
+++ Bank_account.cpp
@@ -1,7 +1,6 @@
 
 #include "Bank_account.h"
 #include <iostream>
-
 using namespace std;
 
 Bank_account::Bank_account(const string &name, const string &number, double balanc)
@@ -24,4 +23,20 @@ void Bank_account::deposite(double n) {
 
 void Bank_account::withdraw(double n) {
 	balance -= n;
+}
+
+int main() {
+	
+	Bank_account bank_account = Bank_account("Adam", "327gj897", 207546.0);
+	bank_account.show();
+
+	cout << "Deposite money" << endl;
+	bank_account.deposite(100.2);
+	bank_account.show();
+
+	cout << "withdraw money" << endl;
+	bank_account.withdraw(50.1);
+	bank_account.show();
+
+	return 0;
 }
