0a63403a8b17b5ac8d4b95c004a266f1f532edd8
Umneiah <omnia.hussien@ymail.com>
2015-10-22 21:15:35 +0200

    Update Bank_account.h

--- Bank_account.h
+++ Bank_account.h
@@ -1,6 +1,6 @@
+
 #ifndef BankAccount_H_
 #define BankAccount_H_
-
 #include <iostream>
 #include <string>
 using namespace std;
@@ -12,10 +12,10 @@ private:
 	double balance;
 
 public:
-	Bank_account();   //default constructor
 	Bank_account(const string &name, const string &number, double balanc);
 	void show();
 	void deposite(double n);
 	void withdraw(double n);
 };
+
 #endif
