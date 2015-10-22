dab936e012b2273366e4840299c730714b8f5725
Umneiah <omnia.hussien@ymail.com>
2015-10-22 21:14:27 +0200

    Update Bank_account.cpp

--- Bank_account.cpp
+++ Bank_account.cpp
@@ -1,1 +1,27 @@
 
+#include "Bank_account.h"
+#include <iostream>
+
+using namespace std;
+
+Bank_account::Bank_account(const string &name, const string &number, double balanc)
+{
+	depositor_name = name;
+	account_number = number;
+	balance = balanc;
+}
+
+
+void Bank_account::show() {
+	cout << "Depositor's name" << depositor_name <<endl
+		<< "Account number" << account_number << endl
+		<< "Balance" << balance << endl;
+}
+
+void Bank_account::deposite(double n) {
+	balance += n;
+}
+
+void Bank_account::withdraw(double n) {
+	balance -= n;
+}
