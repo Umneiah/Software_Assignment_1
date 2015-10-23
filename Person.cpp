b497dbe1946bf242536d1c55191ffafe08943f35
Umneiah <omnia.hussien@ymail.com>
2015-10-23 13:57:16 +0200

    Update Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -1,24 +1,24 @@
 
 #include "Person.h"
 
-Person::Person(const string &ln, const char *fn ) // *fn ya3ny momkn ab3tloh array
+Person::Person(const string &ln, const char *fn) // *fn ya3ny momkn ab3tloh array
 {
-  lname = ln;
-  strcpy(fname, fn); //copy string fn into fname 
+	lname = ln;
+	strcpy_s(fname, fn); //copy string fn into fname 
 };
 
 void Person::Show() const {
- cout << *fname << " " << lname << endl; 
+	cout << *fname << " " << lname << endl;
 };
 
 void Person::FormalShow() const {
- cout << lname << " " << *fname << endl; 
+	cout << lname << " " << *fname << endl;
 };
 
 int main() {
-	Person one; 
-	Person two("SandraBollock"); // use #2 with one default argument
-	Person three("potter", "Harry"); // use #2, no defaults
+	Person one;
+	Person two("Smythecraft"); // use #2 with one default argument
+	Person three("Dimwiddy", "Sam"); // use #2, no defaults
 	one.Show();
 	cout << endl;
 	one.FormalShow();
