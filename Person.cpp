0902a5744d3dd8e1ad0c4c7685a974e137aeaef5
Umneiah <omnia.hussien@ymail.com>
2015-10-23 15:31:47 +0200

    Update Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -19,15 +19,19 @@ int main() {
 	Person one;
 	Person two("Smythecraft"); // use #2 with one default argument
 	Person three("Dimwiddy", "Sam"); // use #2, no defaults
+	
 	one.Show();
 	cout << endl;
 	one.FormalShow();
-
+	cout << endl;
+        
 	two.Show();
+	cout << endl;
 	two.FormalShow();
 	cout << endl;
 
 	three.Show();
+	cout << endl;
 	three.FormalShow();
 	cout << endl;
 
