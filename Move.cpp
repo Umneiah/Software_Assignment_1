edc99cf57a22b2b0f769681c30e9d6da7afae022
Umneiah <omnia.hussien@ymail.com>
2015-10-23 15:02:07 +0200

    Update Move.cpp

--- Move.cpp
+++ Move.cpp
@@ -1,1 +1,23 @@
 
+#include "Header.h"
+
+ Move:: Move(double a , double b) 
+{
+	 x = a;
+	 y = b;
+}
+
+ void Move::showmove() const
+{
+	 cout << "x is " << x << endl;
+	 cout << "y is " << y << endl;
+}
+
+ //add missing , don't know how to write it
+
+ void Move::reset(double a = 0, double b = 0)
+{
+	x = a;
+	y = b;
+}
+
